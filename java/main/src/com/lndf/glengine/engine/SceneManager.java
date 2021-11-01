3d0de39f5cb09632076093f3b910b93dea541c46
Lander Gallastegi <landergallastegi@gmail.com>
2021-11-01 14:12:35 +0100

    Fixed SceneManager clear scenes on termination

--- java/main/src/com/lndf/glengine/engine/SceneManager.java
+++ java/main/src/com/lndf/glengine/engine/SceneManager.java
@@ -8,6 +8,10 @@ public class SceneManager {
 	
 	public static HashSet<Scene> scenes = new HashSet<Scene>();
 	
+	static {
+		Window.addTerminateRunnable(() -> SceneManager.scenes.clear());
+	}
+	
 	public static void updateScenes() {
 		for (Scene scene : SceneManager.scenes) {
 			scene.update();
