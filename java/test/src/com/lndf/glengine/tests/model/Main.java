b176f5e7c1ee837139c474f291b52f48ff31be07
Lander Gallastegi <landergallastegi@gmail.com>
2021-11-09 22:55:23 +0100

    Remove VHACD version property

--- java/test/src/com/lndf/glengine/tests/model/Main.java
+++ java/test/src/com/lndf/glengine/tests/model/Main.java
@@ -15,7 +15,7 @@ public class Main {
 	public static void main(String[] args) {
 		Window win = Window.createWindow("AAA", 800, 600, true);
 		Scene scene = new Scene();
-		Model model = new Model(new Asset("resource:/model/demomodel.fbx"));
+		Model model = new Model(new Asset("resource:/model/Emilioteca.fbx"));
 		GameObject camObj = new GameObject();
 		FPCamera cam = new FPCamera((float) Math.PI / 2.0f, 1000.0f);
 		DirectionalLight light = new DirectionalLight(new Vector3f(.5f,.5f,.5f));
@@ -23,7 +23,7 @@ public class Main {
 		camObj.addComponent(light);
 		camObj.addComponent(light);
 		GameObject persona = model.createGameObject();
-		persona.getTransform().setScale(new Vector3f(0.01f, 0.01f, 0.01f));
+		//persona.getTransform().setScale(new Vector3f(0.01f, 0.01f, 0.01f));
 		scene.addObject(persona);
 		scene.addObject(camObj);
 		win.addDrawable(cam);
